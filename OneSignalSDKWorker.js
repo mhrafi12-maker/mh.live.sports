b47629abae4f7de66db5928209f5ae2503cd9600
mhrafi12-maker <tmdmilon80@gmail.com>
2026-06-23 12:22:54 +0600

    OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-Integrate the OneSignal Web SDK into this codebase.
-
-Follow the instructions at: https://documentation.onesignal.com/docs/en/web-sdk-setup
-
-App ID: 7d3694c5-0de9-4fd0-9cc1-ac1d9e0d4e40
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
